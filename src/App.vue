2400ac591e540e71205443387d63b52c395a7e3b
Mikolaj Figurski <meek.f80@gmail.com>
2021-01-28 15:42:07 -0500

    Make basic pie chart

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,43 @@
   <div id="app">
     <SvgDefinitions />
     <LineGraph :chartData="points" :onSelect="onSelect" />
+    <div id="details" v-if="details !== null">
+      <PieChart :chartData="details" />
+    </div>
   </div>
 </template>
 
 <script>
-import LineGraph from "./components/LineGraph.vue";
 import SvgDefinitions from "./components/SvgDefinitions.vue";
+import LineGraph from "./components/LineGraph.vue";
+import PieChart from "./components/PieChart.vue";
 
 export default {
   name: "App",
   components: {
-    LineGraph,
     SvgDefinitions,
+    LineGraph,
+    PieChart,
   },
   data: () => ({
     points: [
-      { time: 5 * 3600, total: 11, negative: 6420, positive: 4423 },
-      { time: 6 * 3600, total: 10, negative: 4320, positive: 3243 },
-      { time: 7 * 3600, total: 20, negative: 6892, positive: 7523 },
-      { time: 8 * 3600, total: 15, negative: 10098, positive: 4523 },
-      { time: 9 * 3600, total: 19, negative: 7000, positive: 5552 },
-      { time: 10 * 3600, total: 19, negative: 6787, positive: 8960 },
-      { time: 11 * 3600, total: 19, negative: 9298, positive: 8142 },
-      { time: 12 * 3600, total: 19, negative: 11982, positive: 9182 },
-      { time: 13 * 3600, total: 19, negative: 10492, positive: 7523 },
-      { time: 14 * 3600, total: 19, negative: 12839, positive: 5153 },
+      { time: 5 * 3600, total: 50000, negative: 6420, positive: 4423 },
+      { time: 6 * 3600, total: 40000, negative: 4320, positive: 3243 },
+      { time: 7 * 3600, total: 40000, negative: 6892, positive: 7523 },
+      { time: 8 * 3600, total: 50000, negative: 10098, positive: 4523 },
+      { time: 9 * 3600, total: 40000, negative: 7000, positive: 5552 },
+      { time: 10 * 3600, total: 40000, negative: 6787, positive: 8960 },
+      { time: 11 * 3600, total: 50000, negative: 9298, positive: 8142 },
+      { time: 12 * 3600, total: 40000, negative: 11982, positive: 9182 },
+      { time: 13 * 3600, total: 40000, negative: 10492, positive: 7523 },
+      { time: 14 * 3600, total: 40000, negative: 12839, positive: 5153 },
     ],
-    selected: null,
+    details: { time: 5 * 3600, total: 50000, negative: 6420, positive: 4423 },
   }),
   methods: {
     onSelect(i) {
       console.log(`clicked point at ${i}`);
+      this.details = this.points[i];
     },
   },
 };
@@ -43,6 +49,9 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+body {
+  overflow-x: hidden;
 }
 
 #app {
---
+++ src/components/PieChart.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="d3PieChart"></div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+  name: "PieChart",
+  props: ["chartData"],
+  mounted() {
+    this.drawGraph();
+  },
+  methods: {
+    prepareData() {
+      return d3
+        .pie()
+        .sort(null)
+        .value((d) => d.value)([
+        { label: "positive", value: this.chartData.positive },
+        { label: "negative", value: this.chartData.negative },
+        {
+          label: "total",
+          value:
+            this.chartData.total -
+            this.chartData.positive -
+            this.chartData.negative,
+        },
+      ]);
+    },
+    drawGraph() {
+      console.log("drawing graph");
+      const data = this.prepareData();
+      const width = 400;
+      const height = 400;
+      const radius = 200;
+
+      console.log(data);
+      // set up canvas
+      d3.selectAll("#d3PieChart > *").remove();
+      const svg = d3
+        .select("#d3PieChart")
+        .append("svg")
+        .attr("height", height)
+        .attr("width", width)
+        .attr("viewBox", [-width / 2, -height / 2, width, height]);
+      console.log(svg);
+
+      const arc = d3
+        .arc()
+        .innerRadius(radius / 2)
+        .outerRadius(radius);
+      svg
+        .append("g")
+        .selectAll("path")
+        .data(data)
+        .join("path")
+        .attr("class", (d) => `slice ${d.data.label}`)
+        .attr("d", arc);
+    },
+  },
+};
+</script>
+
+<style>
+#d3PieChart .positive {
+  fill: rgb(55, 180, 76);
+}
+#d3PieChart .negative {
+  fill: #ff1744;
+}
+#d3PieChart .total {
+  fill: #eee;
+}
+</style>

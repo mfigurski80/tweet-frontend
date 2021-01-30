fbf6c0c7a6d8a2a7353f7b1fdb732adcba3eca7a
Mikolaj Figurski <meek.f80@gmail.com>
2021-01-30 16:07:14 -0500

    Fix bug where piechart shadow was above nav

--- src/components/NavSection.vue
+++ src/components/NavSection.vue
@@ -24,6 +24,7 @@ export default {
 section {
   border-bottom: 1px solid #eee;
   position: sticky;
+  z-index: 100;
   top: 0;
   /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24); */
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
